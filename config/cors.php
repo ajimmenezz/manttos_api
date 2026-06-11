095eda0a37b471a1028daabdb2248483082be289
Alonso Jimenez <alonso@setpay.mx>
2026-06-10 22:48:28 -0600

    Enable cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://mantenimientos.siccobsolutions.com.mx'],
 
     'allowed_origins_patterns' => [],
 
